0b0538074607e5bf6f5f92673887d87431bb0637
vishalpawar004 <vishalpawar8924@gmail.com>
2025-05-24 10:03:40 +0530

    Add File

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("Database is Connected SuccessFully")
+})
+
+
+
+const app = express()
+
+// // to make input as json
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
+})
+
+// import routes
+import authRouter from "./routes/auth.route.js"
+import noteRouter from "./routes/note.route.js"
+
+app.use("/api/auth", authRouter)
+app.use("/api/note", noteRouter)
+6669696
+// error handling
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+  const message = err.message || "Internal Serer Error"
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+})
